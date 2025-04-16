671817a404e4cd9a377ce2997053458924a7ae0b
harshitdynamite@gmail.com <harshitdynamite@gmail.com>
2025-04-16 17:27:06 +0400

    Added some structure + red border for debugging

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,46 @@
-import AnalogClock from './components/AnalogClock';
+import { Grid, GridItem } from "@chakra-ui/react";
+import { useBreakpointValue } from "@chakra-ui/react";
+// import AnalogClock from "./components/AnalogClock";
 
-function App(){
-  return <div><AnalogClock /></div>
+// function App(){
+//   return <div><AnalogClock /></div>
+// }
+
+function App() {
+  const currentLayout = useBreakpointValue({
+    base: "base",
+    sm: "sm",
+    md: "md",
+    lg: "lg",
+    xl: "xl",
+    "2xl": "2xl",
+  });
+
+  console.log("Current breakpoint:", currentLayout);
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav " "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+      templateRows={{ base: "auto 1fr", lg: "auto 1fr" }}
+      templateColumns={{ base: "1fr", lg: "200px 1fr" }}
+      height="400px"
+      width="300px"
+      gap="1"
+    >
+      <text>Breakpoint: {currentLayout}</text>
+      <GridItem area="nav" bg="coral" padding="4">
+        Nav
+      </GridItem>
+      {/* <GridItem area="aside" bg="black" padding="4">
+        Aside
+      </GridItem> */}
+      <GridItem area="main" bg="blue" padding="4">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
