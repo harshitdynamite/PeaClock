d29aa99bf785e32ae785618d929a41e2eea67f6c
harshitdynamite@gmail.com <harshitdynamite@gmail.com>
2025-04-17 02:17:20 +0400

    Added PeaClock icons

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,24 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
-import logo from "../assets/logo.webp";
+import icon16 from "../assets/peaclock_16x16.png";
+import icon32 from "../assets/peaclock_32x32.png";
+import icon48 from "../assets/peaclock_48x48.png";
+import icon128 from "../assets/peaclock_128x128.png";
 
 const NavBar = () => {
   return (
     <HStack>
-      <Image src={logo} boxSize="30px"></Image>
+      <Image
+        boxSize="30px"
+        src={icon32} // fallback
+        srcSet={`
+    ${icon16} 16w,
+    ${icon32} 32w,
+    ${icon48} 48w,
+    ${icon128} 128w
+  `}
+        sizes="30px"
+        alt="PeaClock logo"
+      />
       <Text>PeaClock</Text>
     </HStack>
   );
